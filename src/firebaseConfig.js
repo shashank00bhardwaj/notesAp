7eae899ac938dee2441c75b3fe1b13bdd343309c
shashank00bhardwaj <98531138+shashank00bhardwaj@users.noreply.github.com>
2022-04-12 17:20:16 +0530

    dsfsfd

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCaiEUl6isAwhr29uX0sNesQpdwiiZ5CRs",
+  authDomain: "noteskeeper-6bca4.firebaseapp.com",
+  projectId: "noteskeeper-6bca4",
+  storageBucket: "noteskeeper-6bca4.appspot.com",
+  messagingSenderId: "53990015210",
+  appId: "1:53990015210:web:e8380cd0b5690f1294d793"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
